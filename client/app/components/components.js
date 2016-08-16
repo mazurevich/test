b7ec43108e214ef0dd1961110408b0582b6e10f4
mazurevich <mazyrevich@gmail.com>
2016-08-16 16:41:26 +0300

    initial state.
    changed changed styl to scss

---
+++ client/app/components/components.js
@@ -0,0 +1,14 @@
+import angular from 'angular';
+import Home from './home/home';
+import About from './about/about';
+
+let componentModule = angular.module('app.components', [
+  Home,
+  About
+])
+  
+.name;
+
+console.log(componentModule);
+
+export default componentModule;
